48742c672dd2c8073ec349cd29664461133c1970
varnikaj29 <varnikaj77@gmail.com>
2019-08-11 17:06:36 +0530

    my name is

--- app/src/main/java/com/example/a/MainActivity.java
+++ app/src/main/java/com/example/a/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.a;
+ package com.example.a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //this is my test code
     }
 }
